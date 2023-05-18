8c1bbecf6dc92c3a1f36e8be8159c01ece3813b7
bogdan <bok@ctrweb.ru>
2023-05-19 03:01:49 +0600

    add mock data

---
+++ src/mocs/sneakers.ts
@@ -0,0 +1,82 @@
+export const SNEAKERS = [
+  {
+    image: '1',
+    title: 'New Balance 550 White Green',
+    price: '185',
+  },
+  {
+    image: 'sneakers2',
+    title: 'Jordan 1 Mid True Blue',
+    price: '144',
+  },
+  {
+    image: 'sneakers3',
+    title: 'Jordan 1 MidJordan 1 Retro High OG Atmosphere (W)',
+    price: '198',
+  },
+  {
+    image: 'sneakers4',
+    title: 'Nike Air Force 1 Low \'07 White',
+    price: '88',
+  },
+  {
+    image: 'sneakers5',
+    title: 'Nike Dunk Low Grey Fog',
+    price: '143',
+  },
+  {
+    image: 'sneakers6',
+    title: 'Nike Title 1',
+    price: '355',
+  },
+  {
+    image: 'sneakers7',
+    title: 'Nike Title 7',
+    price: '80',
+  },
+  {
+    image: 'sneakers8',
+    title: 'Nike Title 8',
+    price: '50',
+  },
+  {
+    image: 'sneakers9',
+    title: 'Nike Title 9',
+    price: '230',
+  },
+  {
+    image: 'sneakers10',
+    title: 'Nike Title 10',
+    price: '299',
+  },
+  {
+    image: 'sneakers11',
+    title: 'Nike Title 11',
+    price: '306',
+  },
+  {
+    image: 'sneakers12',
+    title: 'Nike Title 12',
+    price: '402',
+  },
+  {
+    image: 'sneakers13',
+    title: 'Nike Title 13',
+    price: '278',
+  },
+  {
+    image: 'sneakers14',
+    title: 'Nike Title 14',
+    price: '130',
+  },
+  {
+    image: 'sneakers15',
+    title: 'Nike Title 15',
+    price: '168',
+  },
+  {
+    image: 'sneakers16',
+    title: 'Nike Title 16',
+    price: '260',
+  },
+];
