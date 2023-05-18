ea7591d74440eddffacb166082601da7d2516ada
bogdan <bok@ctrweb.ru>
2023-05-18 23:49:41 +0600

    change import for styles.scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Main from '@pages/Main';
 
-import './style.scss';
+import '@theme/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Main />);
