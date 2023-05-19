a7f2fda3de81fac6327d2a883b1000363001c839
bogdan <bok@ctrweb.ru>
2023-05-19 12:01:13 +0600

    add NotFound page

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import cn from 'classnames';
+
+import Layout from '@components/Layout';
+import Link from "@components/UI/Link";
+
+import { Paths } from '@utils/constants/routes';
+import classes from './NotFound.module.scss';
+
+const NotFound = () => {
+  return (
+    <Layout variant="Center">
+      <div className={classes.NotFound}>
+        <h2 className={cn(classes.Title, 'title')}>Page was not Found!</h2>
+
+        <Link className={classes.Link} url={Paths.products}>Back to the Products</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default NotFound;
