eb3816481da8e4e9af5dc1a0052adf4c998494cd
bogdan <bok@ctrweb.ru>
2023-05-23 20:00:41 +0600

    add Provider to index.ts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import Main from '@pages/Main';
 
+import { store } from '@store/rootStore';
 import '@theme/styles.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
-      <Main />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Main />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
